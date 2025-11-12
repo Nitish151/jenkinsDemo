40cde67264f8878480b09de73e5cae07a660cff8
Nitish151 <nitish.sharma22@spit.ac.in>
2025-11-12 23:59:21 +0530

    first commit

---
+++ HelloJenkins.java
@@ -0,0 +1,7 @@
+public class HelloJenkins {
+    public static void main(String[] args) {
+        System.out.println("🚀 Jenkins Pipeline Demo Running Successfully!");
+        System.out.println("Hello from Jenkins + Java CI/CD pipeline!");
+    }
+}
+ 
